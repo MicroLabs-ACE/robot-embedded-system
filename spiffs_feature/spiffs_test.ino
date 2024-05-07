ff23066c805ee6d677f1b8aaa2a56bad874d009b
computer world <faradayotuoniyo@gmail.com>
2024-05-07 14:11:18 +0100

    feat: add code to save,retreive and delete data to esp32 flash memory new feature allows the possibility of working with the esp32 spi flash memory

---
+++ spiffs_feature/spiffs_test.ino
@@ -0,0 +1,49 @@
+#include "SPIFFS.h"
+ 
+void setup() {
+  Serial.begin(115200);
+  
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  
+  File file = SPIFFS.open("/text.txt");
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  
+  Serial.println("File Content:");
+  while(file.available()){
+    Serial.write(file.read());
+  }
+
+  SPIFFS.remove("/text.txt");
+ 
+  Serial.println("\n\n---AFTER REMOVING---");
+  listAllFiles();
+  file.close();
+}
+ 
+void loop() {
+
+}
+
+
+
+void listAllFiles(){
+ 
+  File root = SPIFFS.open("/");
+ 
+  File file = root.openNextFile();
+ 
+  while(file){
+ 
+      Serial.print("FILE: ");
+      Serial.println(file.name());
+ 
+      file = root.openNextFile();
+  }
+ 
+}
