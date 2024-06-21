3268be3b687e05e239dc58a0863e4436c4dcce41
Victor Momodu <victoramomodu@gmail.com>
2024-06-21 03:13:41 +0100

    style: Format main.cpp

--- robot-embedded-system/src/MapGraph/main.cpp
+++ robot-embedded-system/src/MapGraph/main.cpp
@@ -1,35 +1,47 @@
 #include "MapGraph.hpp"
 #include <iostream>
 
-void testDistance(MapGraph& graph, const std::string& src, const std::string& dst) {
+void testDistance(MapGraph &graph, const std::string &src, const std::string &dst)
+{
     int distance = graph.getDistance(src, dst);
     std::cout << "Distance between " << src << " and " << dst << ": ";
-    if (distance != -1) {
+    if (distance != -1)
+    {
         std::cout << distance << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "No path exists" << std::endl;
     }
 }
 
-void testDirection(MapGraph& graph, const std::string& src, const std::string& dst) {
+void testDirection(MapGraph &graph, const std::string &src, const std::string &dst)
+{
     int direction = graph.getDirection(src, dst);
     std::cout << "Direction from " << src << " to " << dst << ": ";
-    if (direction != -1) {
+    if (direction != -1)
+    {
         std::cout << direction << " degrees" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "Invalid direction" << std::endl;
     }
 }
 
-void testGraphConstructionAndTraversal() {
+void testGraphConstructionAndTraversal()
+{
     MapGraph graph(3, 3);
 
     // Print the generated graph or its part
     std::cout << "Generated Graph:" << std::endl;
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
             std::string node = std::to_string(i) + "-" + std::to_string(j);
-            if (graph.doesNodeExist(node)) {
+            if (graph.doesNodeExist(node))
+            {
                 std::cout << "Node " << node << " is connected to:";
                 // For demonstration purposes, we simulate adjacency here
                 std::cout << " (0-0, weight 3) (0-1, weight 4) (0-2, weight 5)"; // Adjust as per your graph structure
@@ -55,8 +67,8 @@ void testGraphConstructionAndTraversal() {
     testDirection(graph, "2-2", "0-0");
 }
 
-int main() {
+int main()
+{
     testGraphConstructionAndTraversal();
-
     return 0;
 }
