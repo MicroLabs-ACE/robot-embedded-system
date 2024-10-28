44532117721a79702ea649dc099f02fbd4a854b4
y0r0k0b1 <jadunoluwa@gmail.com>
2024-10-28 02:21:27 +0100

    feat: Add to Map component.

--- robot-web-application/src/components/Map.jsx
+++ robot-web-application/src/components/Map.jsx
@@ -1,10 +1,59 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import p5 from "p5";
 import { MapGraph } from "../utils/mapGraph.js";
 
 export default function Map() {
+  const [isWebsocket, setIsWebsocket] = useState(false);
   const sketchRef = useRef(null);
   const graphRef = useRef(null);
+  const socketRef = useRef(null);
+  const isWebsocketRef = useRef(false);
+
+  useEffect(() => {
+    const socket = new WebSocket(`ws://192.168.4.1/ws`);
+    socketRef.current = socket;
+
+    socket.addEventListener("open", () => {
+      setIsWebsocket(true);
+      isWebsocketRef.current = true;
+      console.log("Opened websocket");
+    });
+
+    socket.addEventListener("error", (error) => {
+      setIsWebsocket(false);
+      isWebsocketRef.current = false;
+      console.error("WebSocket Error:", error);
+    });
+
+    socket.addEventListener("message", (event) => {
+      console.log("Received:", event.data);
+    });
+
+    socket.addEventListener("close", () => {
+      setIsWebsocket(false);
+      isWebsocketRef.current = false;
+      console.log("Closed websocket");
+    });
+
+    return () => {
+      if (socketRef.current) {
+        socketRef.current.close();
+      }
+    };
+  }, []);
+
+  const sendCommand = (northSouth, eastWest) => {
+    if (
+      isWebsocketRef.current &&
+      socketRef.current?.readyState === WebSocket.OPEN
+    ) {
+      const command = `${northSouth}${eastWest}2`;
+      socketRef.current.send(command);
+      console.log(`Sent command: ${command}`);
+    } else {
+      console.log("Websocket not connected or ready.");
+    }
+  };
 
   useEffect(() => {
     const CANVAS_LENGTH = 600;
@@ -29,7 +78,7 @@ export default function Map() {
             p.circle(
               UNIT_LENGTH * x + X_OFFSET,
               UNIT_LENGTH * y + Y_OFFSET,
-              CIRCLE_RADIUS,
+              CIRCLE_RADIUS
             );
           }
         }
@@ -40,6 +89,12 @@ export default function Map() {
     return () => {
       p5Instance.remove();
     };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => sendCommand("N", "O"), 2500);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return <div ref={sketchRef} />;
