9ea5a6cc977ca2b4169fb4172f91872ae0fffe29
Victor Momodu <victoramomodu@gmail.com>
2024-10-28 01:20:51 +0100

    feat: Add Map component.

--- robot-web-application/src/pages/HomePage.jsx
+++ robot-web-application/src/pages/HomePage.jsx
@@ -3,14 +3,14 @@ import Controls from "../components/Controls";
 import Navigation from "../components/Navigation";
 import Map from "../components/Map";
 
-const Home = () => {
+const ControllerPage = () => {
   return (
     <div className="flex px-[2rem] items-start w-full md:px-0 md:items-center lg:items-stretch justify-center py-28 flex-col-reverse gap-10 lg:gap-0 relative lg:justify-between lg:w-[1000px] min-h-full lg:flex-row ">
       <Controls />
       <Map />
-      <Arrow rotation={90} />
+      <Navigation />
     </div>
   );
 };
 
-export default Home;
+export default ControllerPage;
